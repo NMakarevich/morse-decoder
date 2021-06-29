cee34df44ca16e180ff0bcfb2dc7f729d8dfdcc9
Nikolai Makarevich <nikoladze529@mail.ru>
2021-06-29 19:41:27 +0300

    feat: solve morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    return expr.split('**********').map(item => {
+        let letters = [];
+        for (let i = 0; i < item.length / 10; i++) {
+          let binary = item.slice(i * 10, i * 10 + 10);
+          binary = binary.slice(binary.indexOf('1'));
+      
+          if (binary.length > 2) {
+            let morse = [];
+            for (let i = 0; i < binary.length / 2; i++) {
+                morse.push(binary.slice(i * 2, i * 2 + 2) == '11' ? '-' : '.');
+            }
+            morse = morse.join('');
+          } else {
+            morse = binary == '11' ? '-' : '.';
+          }
+          letters.push(MORSE_TABLE[morse]);
+        }
+        return letters.join(''); 
+      }).join(' ');
 }
 
 module.exports = {
